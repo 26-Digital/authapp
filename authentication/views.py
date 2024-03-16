5118c5569187bf18d392be925ce8fa476a9c5620
bopaki <btebalo@gmail.com>
2024-03-16 11:25:39 +0200

    Added logout view

--- authentication/views.py
+++ authentication/views.py
@@ -7,7 +7,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.views import TokenObtainPairView
-
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from .models import User
 from .serializers import UserSerializer
 
@@ -26,19 +26,49 @@ class RegisterView(APIView):
             return response
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        try:
+            refresh_token = request.COOKIES.get('refresh_token')
+            token = RefreshToken(refresh_token)
+            token.blacklist()  # Blacklist the refresh token
+        except Exception:
+            pass  # Handle exceptions if needed
+
+        # Clear the authentication cookies
+        response = Response({'message': 'Logout successful'})
+        response.delete_cookie('access_token')
+        response.delete_cookie('refresh_token')
+
+        return response
+
 class LoginView(TokenObtainPairView):
     permission_classes = [AllowAny]
 
-    def get_token_data(self, user):
-        data = super().get_token_data(user)
-        roles = [role.name for role in user.roles.all()]
-        data['roles'] = roles
-        return data
+    def get_user(self, access_token):
+        # Decode the access token to get the user ID
+        from rest_framework_simplejwt.tokens import AccessToken
+        token = AccessToken(access_token)
+        user_id = token.payload['user_id']
+
+        # Retrieve the user instance from the database
+        user = User.objects.get(pk=user_id)
+        return user
 
     def post(self, request, *args, **kwargs):
         response = super().post(request, *args, **kwargs)
         refresh_token = response.data['refresh']
         set_cookies(response, RefreshToken(refresh_token))
+
+        # Get the authenticated user instance
+        user = self.get_user(response.data['access'])
+
+        if user is not None:
+            roles = [role.name for role in user.roles.all()]
+            response.data['roles'] = roles
+
         return response
 
 class TeacherView(APIView):
@@ -59,6 +89,6 @@ def set_cookies(response, refresh):
     access_token = str(refresh.access_token)
     response.set_cookie('access_token', access_token, httponly=True, samesite='Lax')
     response.set_cookie('refresh_token', str(refresh), httponly=True, samesite='Lax')
-    response.data = {
+"""    response.data = {
         'roles': response.data.get('roles', []),
-    }
+    }"""
